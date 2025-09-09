9c03eb70e577361c585b430c4357bd417f7a14e8
DenisPapushaJava <denispapusha0@gmail.com>
2025-09-10 00:35:56 +0300

    added wind paricle

--- script/base_wind.gd
+++ script/base_wind.gd
@@ -1,19 +1,19 @@
 extends Area2D
 @export var wind_force: float = 500
 @export var wind_direction: Vector2 = Vector2.LEFT
+@onready var wind_particle: GPUParticles2D = $WindParticle
 @export var is_active: bool = true
 
 var _bodies_in_area: Array[RigidBody2D] = []
 
-
+signal wind_toggled(active:bool)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	wind_direction = wind_direction.normalized()
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
-
-
+	_update_state()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if !is_active:
@@ -34,4 +34,12 @@ func _on_body_exited(body: Node) -> void:
 		_bodies_in_area.erase(body)
 
 func set_active(active: bool) -> void:
+	if is_active == active:
+		return
 	is_active = active
+	_update_state()
+	emit_signal("wind_toggled", is_active)
+func _update_state() -> void:
+	wind_particle.emitting = is_active
+	
+	
